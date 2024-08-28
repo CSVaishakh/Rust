9f1fdb9d499256cdb2a76a934f752f129d822813
C S Vaishakh <vaishakhsubramonian@gmail.com>
2024-08-28 15:07:11 +0530

    commit

--- rustlings/exercises/02_functions/functions1.rs
+++
@@ -1,7 +0,0 @@
-// TODO: Add some function with the name `call_me` without arguments or a return value.
-fn call_me(){
-    println!("HelloWorld-")
-}
-fn main() {
-    call_me(); // Don't change this line
-}
