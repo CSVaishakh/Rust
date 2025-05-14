bcc519cda331235d028b543cca5f81fcb367828a
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-05-14 05:52:03 +0000

    commit

--- helloworld/target/debug/HelloWorld.d
+++ helloworld/target/debug/HelloWorld.d
@@ -1,1 +1,1 @@
-/home/vaishakh/Code/Rust/helloworld/target/debug/HelloWorld: /home/vaishakh/Code/Rust/helloworld/src/main.rs
+/home/vaishakh/Projects/Rust/helloworld/target/debug/HelloWorld: /home/vaishakh/Projects/Rust/helloworld/src/main.rs
